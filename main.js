10e44d88f26ef1d4107bc6c375e86ae33b239fbe
Ian White <ian.white@t-3.com>
2015-12-19 23:40:09 -0600

    create new module

--- main.js
+++ main.js
@@ -1,40 +1,60 @@
 var EVT = new EventEmitter2();
 
-function timeEvent(data) {
-    console.log('data ' , data);
-    // call WT function here
-}
-
-function youTubeIframeAPIReady(data) {
-    console.log('youTubeIframeAPIReady');
-    $('body').on(
-        'click', 
-        '[rel="js-youtube-play"]', 
-        (e) => { 
-            var videoId = $(e.target).data('vid-id');
-            EVT.emit('createPlayer', videoId)
+(($,window) => {
+    var CustomYoutubeFunctionsModule = (() => {
+        function timeEvent(data) {
+            console.log('data ' , data);
+            // call WT function here
         }
-    );       
-}
 
-function youtubePlayerReady() {
-    console.log('youtubePlayerReady');
-}
+        function youTubeIframeAPIReady(data) {
+            console.log('youTubeIframeAPIReady');
+            $('body').on(
+                'click', 
+                '[rel="js-youtube-play"]', 
+                (e) => { 
+                    var videoId = $(e.target).data('vid-id');
+                    EVT.emit('createPlayer', videoId)
+                }
+            );       
+        }
 
-function YTPlayerStartPLAYING() {
-    console.log('YTPlayerStartPLAYING');
-}
+        function youtubePlayerReady() {
+            console.log('youtubePlayerReady');
+        }
 
-function YTPlayerInit() {
-    console.log('YTPlayerInit');
-}
+        function YTPlayerStartPLAYING() {
+            console.log('YTPlayerStartPLAYING');
+        }
+
+        function YTPlayerInit() {
+            console.log('YTPlayerInit');
+        }
+
+        var publicAPI = {
+            timeEvent,
+            youtubePlayerReady,
+            YTPlayerInit,
+            YTPlayerStartPLAYING,
+            youTubeIframeAPIReady
+        }
+
+        return publicAPI;
+    })();
+
+    if(typeof module !== 'undefined' && module.exports) {
+        module.exports = CustomYoutubeFunctionsModule;
+    } else {
+        window.CustomYoutubeFunctionsModule = CustomYoutubeFunctionsModule;
+    }
+})(jQuery,window);
 
 $(document).ready(() => {
     YoutubeModule.init();
     EVT.emit('init');
-    EVT.on('timeEvent', timeEvent);
-    EVT.on('youTubeIframeAPIReady', youTubeIframeAPIReady);
-    EVT.on('youtubePlayerReady', youtubePlayerReady);
-    EVT.on('YTPlayerStartPLAYING', YTPlayerStartPLAYING);
-    EVT.on('YTPlayerInit', YTPlayerInit);
+    EVT.on('timeEvent', CustomYoutubeFunctionsModule.timeEvent);
+    EVT.on('youTubeIframeAPIReady', CustomYoutubeFunctionsModule.youTubeIframeAPIReady);
+    EVT.on('youtubePlayerReady', CustomYoutubeFunctionsModule.youtubePlayerReady);
+    EVT.on('YTPlayerStartPLAYING', CustomYoutubeFunctionsModule.YTPlayerStartPLAYING);
+    EVT.on('YTPlayerInit', CustomYoutubeFunctionsModule.YTPlayerInit);
 });
